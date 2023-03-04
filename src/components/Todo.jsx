a4efa9d4779eb32423e343cfc7a29c2cd32390ff
Reece <trazhme@gmail.com>
2023-03-04 18:59:14 +0800

    added edit input

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,17 @@
+import { createSignal } from "solid-js";
+
 const Todo = ({ todo, update, remove }) => {
+    const [isEditing, setIsEditing] = createSignal(false);
+
+    const edit = () => {
+        setIsEditing((old) => !old);
+    }
+
     return(
         <div className="todo-item">
             {todo.id} {todo.name} {JSON.stringify(todo.status)}
+            { isEditing() ? <input type="text" /> : ''}
+            <button onClick={() => edit()}>edit</button>
             <button onClick={() => update()}>update</button>
             <button onClick={() => remove()}>remove</button>
         </div>
