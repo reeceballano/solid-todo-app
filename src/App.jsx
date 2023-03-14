53b465bcb2d833b6f9bf7f9698ea5aaf2c1dacbe
Reece <trazhme@gmail.com>
2023-03-14 20:07:42 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Todo from "./components/Todo";
 const App = () => {
     const [todo, setTodo] = createSignal('');
     const [todos, setTodos] = createSignal([
-        { id: 1, name: 'Todo 1', status: false },
-        { id: 2, name: 'Todo 2', status: false },
-        { id: 3, name: 'Todo 3', status: true },
+        { id: 1, name: 'Kain', status: false },
+        { id: 2, name: 'Tulog', status: false },
+        { id: 3, name: 'Gala', status: true },
     ])
 
     const addTodo = (e) => {
@@ -40,7 +40,7 @@ const App = () => {
         <div className="app">
             <div className="app-container">
                 <form className="add-form" onSubmit={addTodo}>
-                    <input type="text" placeholder="new todo" value={todo()} onInput={(e) => setTodo(e.target.value)} />
+                    <input type="text" placeholder="New todo" value={todo()} onInput={(e) => setTodo(e.target.value)} />
                 </form>
 
                 <For each={todos()}>
