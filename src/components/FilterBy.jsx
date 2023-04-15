55d1000a113c3ce1814b841cb76712a74a1a7e93
Reece <trazhme@gmail.com>
2023-04-15 08:42:09 +0800

    update radio button style

--- src/components/FilterBy.jsx
+++ src/components/FilterBy.jsx
@@ -2,17 +2,19 @@ const FilterBy = ({onClick}) => {
     return(
         <div className="filter-by">
             <form>
-                <div className="form-group">
-                    <input onClick={onClick} name="filterby" id="all" type="radio" value="All" checked />
-                    <label>All</label>
-                </div>
-                <div className="form-group">
-                    <input onClick={onClick}  name="filterby" id="completed" type="radio" value="Completed" />
-                    <label>Completed</label>
-                </div>
-                <div className="form-group">
-                    <input onClick={onClick}  name="filterby" id="notcomplete" type="radio" value="NotComplete" />
-                    <label>Not Complete</label>
+                <div class="radio-buttons">
+                    <div className="form-group">
+                        <input onClick={onClick} name="filterby" id="all" type="radio" value="All" checked />
+                        <label for="all" >All</label>
+                    </div>
+                    <div className="form-group">
+                        <input onClick={onClick}  name="filterby" id="completed" type="radio" value="Completed" />
+                        <label for="completed">Completed</label>
+                    </div>
+                    <div className="form-group">
+                        <input onClick={onClick}  name="filterby" id="notcomplete" type="radio" value="NotComplete" />
+                        <label for="notcomplete">Not Complete</label>
+                    </div>
                 </div>
             </form>
         </div>
